0ae34453aacc0e7c5e09e1039ec04093c3abe10e
AlanSun <18698525802@163.com>
2020-07-01 09:41:45 +0800

    add 增加系统文件夹打开功能

---
+++ mapper-generator-javafx-core/src/main/java/com/alan344/utils/FileUtils.java
@@ -0,0 +1,39 @@
+package com.alan344.utils;
+
+import com.alan344happyframework.util.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+/**
+ * @author AlanSun
+ * @date 2020/7/1 9:38
+ */
+@Slf4j
+public class FileUtils {
+    /**
+     * 打开输出目录
+     */
+    public static void open(String outDir) {
+        if (StringUtils.isNotEmpty(outDir)) {
+            try {
+                String osName = System.getProperty("os.name");
+                if (osName != null) {
+                    if (osName.contains("Mac")) {
+                        Runtime.getRuntime().exec("open " + outDir);
+                    } else if (osName.contains("Windows")) {
+                        Runtime.getRuntime().exec("cmd /c start " + outDir);
+                    } else {
+                        log.debug("文件输出目录:" + outDir);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        open("D:\\software\\navicat-for-mysql");
+    }
+}
